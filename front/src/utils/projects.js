96218620151184155819cca05c95755c0ef8f26a
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2022-09-20 19:19:36 -0300

    feat(components)adding proyects and cards

---
+++ front/src/utils/projects.js
@@ -0,0 +1,12 @@
+export const pokemonPro = {
+    description:"This proyect was made during Henry bootcamp, it consumes pokeapi. it has features like filters, orders, create new pokemons, view details of them and more.",
+    img:"../../utils/preview-pokemon.jpg",
+    deploy:"https://pokemon-proyect-mu.vercel.app/",
+    repo:"https://github.com/SebastianCalabro/Pokemon-Proyect"
+}
+export const kembaPro = {
+    description:"This was my first grupal project using agile scrum methodology, it has features like , catalog, ordering, registration, payment gateway, shopping cart and favorites. Dashboard available only for administrators wich gives absolute control of the page",
+    img:"",
+    deploy:"",
+    repo:""
+}
