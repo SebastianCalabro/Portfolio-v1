30c213de8bcf1041f8cc4cf12ba627e38b480998
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2022-09-20 13:41:22 -0300

    adding stack tecnologies

--- front/src/components/Tecnologies/Tecnologies.jsx
+++ front/src/components/Tecnologies/Tecnologies.jsx
@@ -1,5 +1,15 @@
 import s from "./Tecnologies.module.css"
 import { FaArrowDown } from "react-icons/fa"
+import { FaReact } from "react-icons/fa"
+import { SiExpress } from "react-icons/si"
+import { SiRedux } from "react-icons/si"
+import { SiSequelize } from "react-icons/si"
+import { SiPostgresql } from "react-icons/si"
+import { AiFillHtml5 } from "react-icons/ai"
+import { DiCss3 } from "react-icons/di"
+import { FaGitAlt } from "react-icons/fa"
+import { GrNode } from "react-icons/gr"
+import { SiJavascript } from "react-icons/si"
 
 const Tecnologies = () => {
     // const techContainer = document.querySelector(".tech_container")
@@ -21,9 +31,46 @@ const Tecnologies = () => {
     return(
         <div id={s.section} className="section">
             <div className={s.box}>
+            <h1 className={s.stack_title}>STACK</h1>
                 <div className={s.tech_container}>
-
+                    <div className={s.each_tech_div}>
+                        <div className={s.top_tech_div}>
+                            <AiFillHtml5 className={s.html_icon}/>
+                        </div>
+                        <span className={s.each_tech_span}>HTML5</span>
+                    </div>
+                    <div className={s.each_tech_div}>
+                        <div className={s.top_tech_div}>
+                            <DiCss3 className={s.css3_icon}/>
+                        </div>
+                        <span className={s.each_tech_span}>CSS3</span>
+                    </div>
+                    <div className={s.each_tech_div}>
+                        <div className={s.top_tech_div}>
+                            <SiJavascript className={s.js_icon}/>
+                        </div>
+                        <span className={s.each_tech_span}>JavaScript</span>
+                    </div>
+                    <div className={s.each_tech_div}>
+                        <div className={s.top_tech_div}>
+                            <FaReact className={s.react_icon}/>
+                        </div>
+                        <span className={s.each_tech_span}>React</span>
+                    </div>
+                    <div className={s.each_tech_div}>
+                        <div className={s.top_tech_div}>
+                            <SiRedux className={s.redux_icon}/>
+                        </div>
+                        <span className={s.each_tech_span}>Redux</span>
+                    </div>
+                    <div className={s.each_tech_div}>
+                        <div className={s.top_tech_div}>
+                            <GrNode className={s.node_icon}/>
+                        </div>
+                        <span className={s.each_tech_span}>Nodejs</span>
+                    </div>
                 </div>
+                <div className={s.br}></div>
             </div>
             <a href="#thirdPage">
             <FaArrowDown className={s.icon_arrow}/>
