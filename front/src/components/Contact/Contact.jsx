6eecec4f14bbababe943777df430167288e077b9
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2022-09-21 12:13:06 -0300

    feat(components/Contact) adding contact component

---
+++ front/src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import s from "./Contact.module.css"
+const Contact = () =>{
+    return(
+        <div id={s.section} className="section">
+            <div className={s.box}>
+                
+            </div>
+        </div>
+    )
+}
+export default Contact
