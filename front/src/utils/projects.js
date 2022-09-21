a8ba3c10b6bcf0fdc56b8491904cdaa22edf7b71
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2022-09-21 00:22:25 -0300

    feat(projects.js)adding project objects

--- front/src/utils/projects.js
+++ front/src/utils/projects.js
@@ -1,12 +1,14 @@
 export const pokemonPro = {
-    description:"This proyect was made during Henry bootcamp, it consumes pokeapi. it has features like filters, orders, create new pokemons, view details of them and more.",
+    title:"Pokémon proyect",
+    description:"This proyect was made during Henry bootcamp, it consumes pokeapi and has features like filters, orders, create new pokémons, view details of them and more.",
     img:"../../utils/preview-pokemon.jpg",
     deploy:"https://pokemon-proyect-mu.vercel.app/",
-    repo:"https://github.com/SebastianCalabro/Pokemon-Proyect"
+    repo:"https://github.com/SebastianCalabro/Pokemon-Proyect",
+    tecnologies:["react","redux","express","sequelize","postgresql","css3","html5", "nodejs", "javascript"]
 }
 export const kembaPro = {
     description:"This was my first grupal project using agile scrum methodology, it has features like , catalog, ordering, registration, payment gateway, shopping cart and favorites. Dashboard available only for administrators wich gives absolute control of the page",
-    img:"",
-    deploy:"",
-    repo:""
+    img:"../../utils/preview-kemba.jpg",
+    deploy:"https://pf-henry.vercel.app/",
+    repo:"https://github.com/SebastianCalabro/PF---Henry"
 }
